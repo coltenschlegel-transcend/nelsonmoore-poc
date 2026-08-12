6f54e459135ecb15e721ba1600f83301a6480b73
allison-transcend <167145904+allison-transcend@users.noreply.github.com>
2026-08-12 13:03:50 -0400

    Fix airgapSrc assignment syntax in brand.js

--- js/brand.js
+++ js/brand.js
@@ -17,7 +17,7 @@ window.NM_BRAND = {
      (https://transcend-cdn.com/cm/<BUNDLE-ID>/airgap.js), commit to main,
      and Pages redeploys in under a minute. See README. */
   airgapSrc:
-    "https://transcend-cdn.com/cm/afb7f6ea-772c-4c18-ac1f-4de68254ab75/airgap.js",
+  src="https://transcend-cdn.com/cm/afb7f6ea-772c-4c18-ac1f-4de68254ab75/airgap.js"
   airgapOverrides: "GoogleConsentMode FacebookLDU",
   privacyCenterUrl: "https://privacy.nelsonmoore.us/",
   privacyPolicyUrl: "https://privacy.nelsonmoore.us/policies",
